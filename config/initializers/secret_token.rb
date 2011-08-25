c3f60512e63460fa3736c3b1074f6f2ad0c859af
Pankhuri <pankhurisa@gmail.com>
2011-08-25 13:24:17 +0530

    Pankhuri - Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bhaji::Application.config.secret_token = '128ce43303edaf90d4b339dee717b1cfdeb44a0e936254f72b2c21ea9931b7594200fc58f1fbcee5d85675e21206c36dc57a29b5da321766e5658c1e31dd7212'
