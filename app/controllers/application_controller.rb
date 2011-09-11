1276f932ba3d95c12d0d2dccf2909842e94b38c8
Pankhuri <pankhurisa@gmail.com>
2011-09-11 23:52:59 +0530

    Pankhuri - [#5] [Introduce User] - Default devise views and user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  layout 'application'
+  layout :layout_by_resource
+
   def home
 
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "plain"
+    else
+      "application"
+    end
   end
 end
